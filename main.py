311752ca19fe3ab158025371e11d3f2815c74ed4
ProfitScannerBot <orenmarchelloxdd@gmail.com>
2026-08-26 17:44:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def activated_menu() -> InlineKeyboardMarkup:
     builder.row(
         InlineKeyboardButton(
             text="📊 OPEN TERMINAL",
-            web_app=WebAppInfo(url="https://github.com/ProfitScannerBot/Profit_ScannerBotapp.git")
+            web_app=WebAppInfo(url="https://profitscannerbot.github.io/Profit_ScannerBotapp/")
         )
     )
     return builder.as_markup()
